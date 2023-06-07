0c56e4b5ac21b6ad58e7602517fa4db0389fbd3f
ryanadhitama <ryanadhitama2@gmail.com>
2023-06-07 15:03:52 +0800

    goroutines with loop

--- goroutines_test.go
+++ goroutines_test.go
@@ -6,12 +6,26 @@ import (
 	"time"
 )
 
+// goroutines
 func RunHelloWorld() {
 	fmt.Println("Hello World")
 }
 
 func TestCreateGoroutine(t *testing.T) {
 	go RunHelloWorld()
-	fmt.Println ("Ups")
+	fmt.Println("Ups")
+	time.Sleep(1 * time.Second)
+}
+
+// goroutines with looping
+func DisplayNumber(number int) {
+	fmt.Println("Number", number)
+}
+
+func TestCreateGoroutineLoop(t *testing.T) {
+	for i := 0; i < 100000; i++ {
+		go DisplayNumber(i)
+	}
+	
 	time.Sleep(1 * time.Second)
 }
