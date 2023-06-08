8afcd7bde8dbd52b7df380931913c2d21ff3b8a5
ryanadhitama <ryanadhitama2@gmail.com>
2023-06-08 08:49:02 +0800

    channel as parameter

--- goroutines_test.go
+++ goroutines_test.go
@@ -46,3 +46,19 @@ func TestCreateChannel(t *testing.T) {
 
 	time.Sleep(5 * time.Second)
 }
+
+// Channel as Parameter
+func GiveMeResponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Ryan Adhitama"
+}
+
+func TestChannelAsParameter(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go GiveMeResponse(channel)
+
+	data := <- channel
+	fmt.Println(data)
+}
