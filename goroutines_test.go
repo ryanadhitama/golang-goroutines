ac4c7e2882aeee7438f1b9a99cc9ae35c61b7d01
ryanadhitama <ryanadhitama2@gmail.com>
2023-06-16 07:12:07 +0800

    test select channel

--- goroutines_test.go
+++ goroutines_test.go
@@ -119,3 +119,35 @@ func TestRangeChannel(t *testing.T) {
 	
 	time.Sleep(2 * time.Second)
 }
+
+// Select channel
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+
+	for {
+		select {
+		case data := <- channel1:
+				fmt.Println("Data dari channel 1", data)
+				counter++
+		case data := <- channel2:
+				fmt.Println("Data dari channel 2", data)
+				counter++
+		}
+
+		if counter == 2 {
+			break;
+		}
+	}
+
+	
+	
+	time.Sleep(2 * time.Second)
+}
